d36345051dca243157e85031e06aa255dc5c55a9
DeveloperSaif04 <saif.coder04@gmail.com>
2024-03-13 06:15:36 +0530

    change

--- models/config.js
+++ models/config.js
@@ -3,7 +3,7 @@ const mongoose= require("mongoose")
 mongoose
 .connect(process.env.DATABASE)
 .then(()=>{
-    console.log("db connected")
+    console.log("db connected", process.env.DATABASE)
 })
 .catch((err)=>{
     console.log(err.message)
